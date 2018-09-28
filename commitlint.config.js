e0ecead64079c486318fe61e8f86c7dc36fd69d7
stevezyyang(杨振宇) <stevezyyang@tencent.com>
2018-09-28 18:14:23 +0800

    chore: avoid using chinese to commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,5 @@ module.exports = {
     // https://www.npmjs.com/package/@commitlint/config-conventional
     extends: ['@commitlint/config-conventional'],
     "rules": {
-        "subject-case": [0],
     },
 }
