b4b9ac50e7cf401b1b475e5ea3c901e349ca396f
stevezyyang <stevezyyang@tencent.com>
2018-12-03 16:12:28 +0800

    chore: add TuaApi prefix to dist's files, add TuaApi.umd.min.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,16 @@ import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
 import { eslint } from 'rollup-plugin-eslint'
+import { uglify } from 'rollup-plugin-uglify'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
-export default {
-    input: 'src/TuaApi.js',
-    output: [{
-        file: 'dist/umd.js',
+const output = {
+    es: {
+        file: 'dist/TuaApi.es.js',
+        format: 'es',
+    },
+    umd: {
+        file: 'dist/TuaApi.umd.js',
         name: 'TuaApi',
         format: 'umd',
         exports: 'named',
@@ -16,19 +20,31 @@ export default {
             axios: 'axios',
             'fetch-jsonp': 'fetchJsonp',
         },
-    }, {
-        file: 'dist/es.js',
-        format: 'es',
-    }],
-    plugins: [
-        eslint(),
-        json(),
-        nodeResolve(),
-        babel(),
-        commonjs(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('prod'),
-        }),
-    ],
-    external: ['axios', 'fetch-jsonp'],
+    },
 }
+const plugins = [
+    eslint(),
+    json(),
+    nodeResolve(),
+    babel(),
+    commonjs(),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify('prod'),
+    }),
+]
+const external = ['axios', 'fetch-jsonp']
+
+export default [{
+    input: 'src/TuaApi.js',
+    output: [ output.es, output.umd ],
+    plugins,
+    external,
+}, {
+    input: 'src/TuaApi.js',
+    output: {
+        ...output.umd,
+        file: 'dist/TuaApi.umd.min.js',
+    },
+    plugins: [ ...plugins, uglify() ],
+    external,
+}]
